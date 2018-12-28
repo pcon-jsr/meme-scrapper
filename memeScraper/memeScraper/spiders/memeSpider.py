5482506a12f2284a009c58470e9d113288f1972f
Adarsh Anurag <35106820+DevAdarshAnurag@users.noreply.github.com>
2018-12-28 10:36:29 +0530

    Add files via upload

---
+++ memeScraper/memeScraper/spiders/memeSpider.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec 26 15:10:15 2018
+
+@author: ADARSH ANURAG
+"""
+from scrapy import Spider
+from scrapy.selector import Selector
+from memeScraper.items import Meme
+
+class MemeSpider(Spider):
+    name ="memebot"
+    allowed_domain = ["https://www.memedroid.com"]
+    start_urls = ["https://www.memedroid.com/memes/latest/"]
+    
+    def parse(self,response):
+        Title=[]
+        Image=[]
+        Tags=[]
+        container = response.xpath("//div[@class='gallery-memes-container']")
+        articles = container.xpath(".//article")
+        for article in articles:
+            title = article.xpath(".//div[@class='item-aux-container']/header/h1/a/text()").extract()
+            Title.append(title[0])
+            image_link = article.xpath(".//div[@class='item-aux-container']/a[@class='dyn-link']/img/@src").extract()
+            Image.append(image_link[0])
+            tag = article.xpath(".//div[@class='tags-container']/a/text()").extract()
+            Tags.append(tag)
+            print('\n')
+        item = Meme()
+        item['title'] = Title
+        item['image'] = Image
+        item['tags'] = Tags
+        return item
